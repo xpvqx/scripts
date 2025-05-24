7ed5cbc55c91c5c7916dfc29e7dce061a3baad2e
averyiaa <aaveryia@gmail.com>
2025-05-24 18:18:15 +0200

    bluetoothctl xm4

--- bluetooth.sh
+++ bluetooth.sh
@@ -1,1 +1,7 @@
-bluetoothctl connect AC:80:0A:5D:CB:ED
+#!/bin/bash
+
+# Bluetooth device MAC address
+DEVICE_MAC="AC:80:0A:5D:CB:ED"
+
+# Connect using bluetoothctl
+echo -e "connect $DEVICE_MAC\nexit" | bluetoothctl
