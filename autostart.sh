b5c449a9dda077f63b0ed1a08279cbf9b2f84bc4
xpvqx <kjs02x@gmail.com>
2025-10-26 16:30:46 +0100

    dwm autostart.sh

--- autostart.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-picom -b
-pipewire &
-pipewire-pulse &
-(sleep 2; /home/karl/.suckless/slstatus/slstatus &) &
--- tailscale_status.sh
+++ tailscale_status.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 export PATH=/usr/bin:/bin:/usr/local/bin
-for i in 1 2 3 4 5; do
-    ip=$(/usr/bin/tailscale ip -4 2>/dev/null)
-    if [ -n "$ip" ]; then
-        echo "$ip"
-        exit
+
+while true; do
+    # Check if Tailscale is running and logged in
+    if tailscale status >/dev/null 2>&1; then
+        ip=$(tailscale ip -4 2>/dev/null)
+        if [ -n "$ip" ]; then
+            echo "$ip"
+        else
+            echo "DOWN"
+        fi
+    else
+        echo "DOWN"
     fi
     sleep 1
 done
-echo "down"
