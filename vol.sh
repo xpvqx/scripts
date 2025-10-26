ce9b3b520453f7b737b57a6333b3fc33cefe15a6
xpvqx <kjs02x@gmail.com>
2025-10-26 16:45:01 +0100

    remove unused scripts

--- vol.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-VOL=$(/usr/bin/pactl get-sink-volume @DEFAULT_SINK@ | /bin/grep 'front-left:' | /usr/bin/awk '{print $5}' | head -n1)
-
-if [ -z "$VOL" ]; then
-  echo "n/a"
-else
-  echo "$VOL"
-fi
